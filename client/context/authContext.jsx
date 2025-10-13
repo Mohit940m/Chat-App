2216bfea4cc688fe99d0e2fdbf0aa7885c76a0d7
Mohit Saha Chowdhdhury <sahachowdhurymohit@gmail.com>
2025-10-14 01:22:27 +0530

    feat: add login and logout auth context

--- client/context/authContext.jsx
+++ client/context/authContext.jsx
@@ -2,6 +2,7 @@ import { createContext, use, useEffect, useState } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
+import { set } from "mongoose";
 
 
 const backendUrl = process.env.BACKEND_URL;
@@ -31,6 +32,41 @@ export const AuthProvider = ({ children }) => {
             }
         }
     }
+
+
+    //login function to handle user authentication and socket connection
+
+    const login = async (state, credentials) => {
+        try {
+            const { data } = await axios.post(`${backendUrl}/api/auth/login`, credentials);
+            if (data.success) {
+                setAuthUser(data.userData);
+                connectSocket(data.userData);
+                axios.defaults.headers.common["token"] = data.token;
+                setToken(data.token);
+                localStorage.setItem("token", data.token);
+                toast.success(data.message);
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
+
+    // logout function to handle user logout and socket disconnection
+
+    const logout = async () => {
+        localStorage.removeItem("token");
+        setToken(null);
+        setAuthUser(null);
+        axios.defaults.headers.common["token"] = null;
+        toast.success("Logout successful");
+        socket?.disconnect();
+    }
+
+
+    // Update user data function to update the user data in the context and local storage
 
     // Connect socket functon to handle socket connection and online users update
     const connectSocket = () => {
